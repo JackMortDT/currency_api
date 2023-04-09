6cfc346360bb783f3d1547b13cca62e04e0ad80f
Luis Sastré 🦎 <soul6496@gmail.com>
2023-04-09 09:08:14 -0600

    Adding logic for save result from web service on db

--- services/rates_service.go
+++ services/rates_service.go
@@ -2,13 +2,14 @@ package services
 
 import (
 	"currency_api/command"
+	"currency_api/domain"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 )
 
-func RequestCurrencyRates() *command.Rates {
+func RequestCurrencyRates() *command.Rate {
 	currencyUrl := os.Getenv("CURRENCY_URL")
 	apiKey := os.Getenv("API_KEY")
 
@@ -17,11 +18,20 @@ func RequestCurrencyRates() *command.Rates {
 		log.Print(err)
 	}
 
-	var rates command.Rates
+	var rates command.Rate
 	err = json.NewDecoder(resp.Body).Decode(&rates)
 	if err != nil {
 		log.Print(err)
 	}
 
+	saveCurrencyResponse(&rates)
+
 	return &rates
+}
+
+func saveCurrencyResponse(rates *command.Rate) {
+	currencyRates := command.ConvertToCurrencyRates(rates)
+	for _, currencyRate := range currencyRates {
+		domain.CurrencyRepo.CreateOrUpdate(currencyRate)
+	}
 }
