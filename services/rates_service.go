5b19fc8052187c8ce50ec8853316245e8630b5f9
Luis Sastré 🦎 <soul6496@gmail.com>
2023-04-09 11:41:07 -0600

    Adding logic on service for custom timeout on request and return errors

--- services/rates_service.go
+++ services/rates_service.go
@@ -3,36 +3,57 @@ package services
 import (
 	"currency_api/command"
 	"currency_api/domain"
+	"currency_api/utils/error_utils"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
-func RequestCurrencyRates() *command.Rate {
+func RequestCurrencyRates() (*command.Rate, error_utils.MessageErr) {
 	currencyUrl := os.Getenv("CURRENCY_URL")
 	apiKey := os.Getenv("API_KEY")
+	stringTimeout := os.Getenv("TIMEOUT")
+	timeout, err := strconv.Atoi(stringTimeout)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error converting timeout from env")
+		log.Print(errorMessage)
+
+		return nil, error_utils.NewInternalServerError(errorMessage)
+	}
+
+	client := &http.Client{Timeout: time.Duration(timeout) * time.Second}
 
 	start := time.Now()
-	resp, err := http.Get(currencyUrl + apiKey)
+	resp, err := client.Get(currencyUrl + apiKey)
 	duration := time.Since(start).Milliseconds()
 
 	if err != nil {
-		log.Print(err)
+		errorMessage := fmt.Sprintf("Error requesting currency API: %s", err)
+		log.Print(errorMessage)
+		saveFailCallRecord(start)
+
+		return nil, error_utils.NewServiceUnavailableError(errorMessage)
 	}
 	defer resp.Body.Close()
 
 	var rates command.Rate
 	err = json.NewDecoder(resp.Body).Decode(&rates)
 	if err != nil {
-		log.Print(err)
+		errorMessage := fmt.Sprintf("Error decoding API response: %s", err)
+		log.Print(errorMessage)
+
+		return nil, error_utils.NewInternalServerError(errorMessage)
+
 	}
 
 	saveCurrencyResponse(&rates)
 	saveCallRecord(start, duration)
 
-	return &rates
+	return &rates, nil
 }
 
 func saveCurrencyResponse(rates *command.Rate) {
@@ -46,5 +67,14 @@ func saveCallRecord(requestDate time.Time, duration int64) {
 	domain.CallRecordRepo.Create(&domain.CallRecord{
 		RequestDate: requestDate,
 		Duration:    duration,
+		Sucess:      true,
 	})
+}
+
+func saveFailCallRecord(requestDate time.Time) {
+	domain.CallRecordRepo.Create(&domain.CallRecord{
+		RequestDate: requestDate,
+		Sucess:      false,
+	})
+
 }
