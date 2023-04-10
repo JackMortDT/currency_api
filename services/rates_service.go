fa904077faa9024dca90774094058ee333347efa
Luis Sastré 🦎 <soul6496@gmail.com>
2023-04-09 18:40:24 -0600

    Refactor of rates service

--- services/rates_service.go
+++ services/rates_service.go
@@ -14,34 +14,17 @@ import (
 )
 
 func RequestCurrencyRates() (*command.Rate, error_utils.MessageErr) {
-	currencyUrl := os.Getenv("CURRENCY_URL")
-	apiKey := os.Getenv("API_KEY")
-	stringTimeout := os.Getenv("TIMEOUT")
-	timeout, err := strconv.Atoi(stringTimeout)
-	if err != nil {
-		errorMessage := fmt.Sprintf("Error converting timeout from env")
-		log.Print(errorMessage)
-
-		return nil, error_utils.NewInternalServerError(errorMessage)
-	}
-
-	client := &http.Client{Timeout: time.Duration(timeout) * time.Second}
 
 	start := time.Now()
-	resp, err := client.Get(currencyUrl + apiKey)
+	resp, requestErr := apiRequest()
+	if requestErr != nil {
+		saveFailCallRecord(start)
+		return nil, requestErr
+	}
 	duration := time.Since(start).Milliseconds()
 
-	if err != nil {
-		errorMessage := fmt.Sprintf("Error requesting currency API: %s", err)
-		log.Print(errorMessage)
-		saveFailCallRecord(start)
-
-		return nil, error_utils.NewServiceUnavailableError(errorMessage)
-	}
-	defer resp.Body.Close()
-
 	var rates command.Rate
-	err = json.NewDecoder(resp.Body).Decode(&rates)
+	err := json.NewDecoder(resp.Body).Decode(&rates)
 	if err != nil {
 		errorMessage := fmt.Sprintf("Error decoding API response: %s", err)
 		log.Print(errorMessage)
@@ -54,6 +37,34 @@ func RequestCurrencyRates() (*command.Rate, error_utils.MessageErr) {
 	saveCallRecord(start, duration)
 
 	return &rates, nil
+}
+
+func apiRequest() (*http.Response, error_utils.MessageErr) {
+	currencyUrl := os.Getenv("CURRENCY_URL")
+	apiKey := os.Getenv("API_KEY")
+	stringTimeout := os.Getenv("TIMEOUT")
+
+	timeout, err := strconv.Atoi(stringTimeout)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error converting timeout from env")
+		log.Print(errorMessage)
+
+		return nil, error_utils.NewInternalServerError(errorMessage)
+	}
+
+	client := &http.Client{Timeout: time.Duration(timeout) * time.Second}
+
+	resp, err := client.Get(currencyUrl + apiKey)
+
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error requesting currency API: %s", err)
+		log.Print(errorMessage)
+
+		return nil, error_utils.NewServiceUnavailableError(errorMessage)
+	}
+	defer resp.Body.Close()
+
+	return resp, nil
 }
 
 func saveCurrencyResponse(rates *command.Rate) {
