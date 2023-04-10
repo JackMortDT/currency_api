ee198c051a2b353da440c0b19389bb42dd8d44eb
Luis Sastré 🦎 <soul6496@gmail.com>
2023-04-10 08:10:37 -0600

    Adding tests for currency dao

--- domain/currency_dao.go
+++ domain/currency_dao.go
@@ -10,6 +10,7 @@ var (
 
 type currencyRepoInterface interface {
 	Initialize(*gorm.DB) *gorm.DB
+	GetAll() ([]CurrencyRate, error)
 	CreateOrUpdate(*CurrencyRate) error
 }
 
@@ -21,6 +22,15 @@ func (cR *currencyRepo) Initialize(db *gorm.DB) *gorm.DB {
 	cR.db = db
 
 	return cR.db
+}
+
+func (cR *currencyRepo) GetAll() ([]CurrencyRate, error) {
+	var currencies []CurrencyRate
+	result := cR.db.Find(&currencies)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return currencies, nil
 }
 
 func (cR *currencyRepo) CreateOrUpdate(currency *CurrencyRate) error {
--- domain/currency_test.go
+++ domain/currency_test.go
@@ -1,1 +1,72 @@
-package domain_test
+package domain
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func TestInitialize(t *testing.T) {
+	dbResult, db, err := fakeDatabaseConnection()
+	assert.NoError(t, err)
+	assert.EqualValues(t, dbResult, db)
+}
+
+func TestCreateOrUpdate(t *testing.T) {
+	db, _, err := fakeDatabaseConnection()
+	assert.NoError(t, err)
+
+	err = db.AutoMigrate(&CurrencyRate{})
+	assert.NoError(t, err)
+
+	// Test with one register on DB
+	currency := &CurrencyRate{
+		Code:      "MXN",
+		Value:     18.10,
+		UpdatedAt: time.Now(),
+	}
+
+	err = CurrencyRepo.CreateOrUpdate(currency)
+	assert.NoError(t, err)
+
+	var result CurrencyRate
+	db.First(&result, "code = ?", "MXN")
+	assert.EqualValues(t, result.Code, currency.Code)
+	assert.EqualValues(t, result.Value, currency.Value)
+
+	// Test adding other register on DB
+	currency = &CurrencyRate{
+		Code:      "USD",
+		Value:     1,
+		UpdatedAt: time.Now(),
+	}
+
+	err = CurrencyRepo.CreateOrUpdate(currency)
+	assert.NoError(t, err)
+
+	var usdResult CurrencyRate
+	db.First(&usdResult, "code = ?", "USD")
+	assert.EqualValues(t, usdResult.Code, currency.Code)
+	assert.EqualValues(t, usdResult.Value, currency.Value)
+
+	// Test for all registers
+	currencies, err := CurrencyRepo.GetAll()
+	assert.NoError(t, err)
+	assert.EqualValues(t, len(currencies), 2)
+
+	db.Exec("DELETE FROM currency_rates")
+
+}
+
+func fakeDatabaseConnection() (*gorm.DB, *gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=currency_test sslmode=disable"
+	db, err := gorm.Open(postgres.New(postgres.Config{DSN: dsn}))
+	if err != nil {
+		return nil, nil, err
+	}
+	dbResult := CurrencyRepo.Initialize(db)
+	return dbResult, db, nil
+}
