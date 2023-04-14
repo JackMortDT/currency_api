62228755d757be41f9ae2246aebfba933d9ff1c4
Luis Sastre <luis.sastre@landonearth.com>
2023-04-14 12:48:23 -0500

    Adding interval request on .env file

--- routines/currency_routine.go
+++ routines/currency_routine.go
@@ -3,18 +3,25 @@ package routines
 import (
 	"currency_api/services"
 	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
 func ExecuteApiRequest() {
 	log.Println("Inicio de proceso de la goroutine")
+	intervalRequest := os.Getenv("INTERVAL_REQUEST")
+	interval, err := strconv.Atoi(intervalRequest)
+	if err != nil {
+		panic("Interval is not set correctly on environment variables")
+	}
 	go func() {
 		for {
 			_, err := services.RatesService.RequestCurrencyRates()
 			if err != nil {
 				log.Println("Ocurrio un error a la hora de ejecutar currency rates")
 			}
-			time.Sleep(10 * time.Second)
+			time.Sleep(time.Duration(interval) * time.Second)
 		}
 	}()
 }
